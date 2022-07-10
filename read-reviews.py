8d4741120a3301584d0af798cbdb980ee8b9689b
Eason Chen <easonchen00@gmail.com>
2022-07-10 15:08:49 +0800

    advanced list comprehension

--- read-reviews.py
+++ read-reviews.py
@@ -20,4 +20,16 @@ for d in data:
 print('一共有', len(new), '筆留言小於100字')  # 16~19 是在做清單的篩選，篩出留言字數小於100字 
 print(new[0])
 
+#good = []
+#for d in data:
+#	if 'good' in d:  #如果評論中有提到good
+#		good.append(d)
+#print('一共有', len(good), '筆提到good')
+
+good = [d for d in data if 'good' in d]  # 承上寫法的進階速寫法，可找出有good的評論
+print(good)
+
+bad = ['bad' in d for d in data]   # 'bad' in d 是一種運算，其運算結果為布林值
+print(bad)
+
 
